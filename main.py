6bf9234d0174899aef0df9926d09fe1bcdd99c95
Sultan-html <shalava1321@gmail.com>
2025-01-08 22:54:11 +0600

    Cделал кнопки

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('fdsfdsf')
+from tkinter import tk
+
+root = tk.Tk()
+root.title('Крестики нолики')
+
+buttons = [[None for _ in range(3)] for _ in range(3)]
+for i in range(3):
+    for j in range(3):
+        buttons[i][j] = tk.Button(root, text="", font=("Arial", 24), width=5, height=2)
+        buttons[i][j].grid(row=i, column=j)
+
+root.mainloop()
