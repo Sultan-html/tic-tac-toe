1ed73df111a0a025c2a0b7a67b809d479501155d
Sultan-html <shalava1321@gmail.com>
2025-01-08 22:56:16 +0600

    Проверка победителя

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ root.title('Крестики нолики')
 
 buttons = [[None for _ in range(3)] for _ in range(3)]
 
+def check_winner():
+    for i in range(3):
+        if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
+            return buttons[i][0]["text"]
+        if buttons[0][i]["text"] == buttons[1][i]["text"] == buttons[2][i]["text"] != "":
+            return buttons[0][i]["text"]
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        return buttons[0][0]["text"]
+    if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        return buttons[0][2]["text"]
+    return None
+
 def make_move(row, col):
     global current_player
     if buttons[row][col]["text"] == "":
